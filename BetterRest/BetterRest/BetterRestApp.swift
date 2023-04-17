ee9ee33e61ce880586523032247f46c279853c65
Elias Breitenbach <elias.b@me.com>
2023-04-17 21:30:20 +0200

    Create a Core ML

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Elias Breitenbach on 16.04.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
