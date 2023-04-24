3b555ed50a6bbf4c598b0ed1021295f6590da3f8
Elias Breitenbach <elias.b@me.com>
2023-04-24 21:03:17 +0200

    Challenge Section/Picker/Button

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -24,34 +24,58 @@ struct ContentView: View {
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
     }
-    
+    struct GrowingButton: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+                .scaleEffect(configuration.isPressed ? 1.2 : 1)
+                .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+        }
+    }
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                Section {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+                        
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                Section {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("Desired amount of sleep")
+                            .font(.headline)
+                        
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    }
                 }
-                VStack(alignment: .leading, spacing: 0) {
+                Section {
+                    Picker("1 Cup", selection: $coffeeAmount) {
+                        ForEach(0..<21) {
+                            Text("\($0) Cups")
+                        }
+                        
+                    }
+                    //                    VStack(alignment: .leading, spacing: 0) {
+                    //                        Text("Daily coffee intake")
+                    //                            .font(.headline)
+                    //                        Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in:  1...20)
+                }  header: {
                     Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in:  1...20)
                 }
+                
+                Button("Calculate", action: calculateBedtime) 
+                    .buttonStyle(GrowingButton())
+                
             }
             .navigationTitle("Better Rest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
+
             .alert(alertTitle, isPresented: $showAlert) {
                 Button("OK") { }
             } message: {
@@ -63,18 +87,18 @@ struct ContentView: View {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
-//            SleepCalculator erstellte Datei in CoreML
+            //            SleepCalculator erstellte Datei in CoreML
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-//            wake: aus der SleepCalculator datei
-//            estimatedSleep: aus der SleepCalculator datei
-//            coffee: aus der SleepCalculator datei
+            //            wake: aus der SleepCalculator datei
+            //            estimatedSleep: aus der SleepCalculator datei
+            //            coffee: aus der SleepCalculator datei
             
             let sleepTime = wakeUp - prediction.actualSleep
-//            actualSleep: aus der SleepCalculator datei
+            //            actualSleep: aus der SleepCalculator datei
             alertTitle = "Your ideal bedtime is..."
             alertessage = sleepTime.formatted(date: .omitted, time: .shortened)
             
